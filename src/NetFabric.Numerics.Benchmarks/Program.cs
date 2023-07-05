2bc1627586cb2e0ddc9e2ae0fbed364de7e54db2
Antao Almada <antao.almada@gmail.com>
2023-07-05 10:23:47 +0100

    Add jobs for .NET 7 and .NET 8

--- src/NetFabric.Numerics.Benchmarks/Program.cs
+++ src/NetFabric.Numerics.Benchmarks/Program.cs
@@ -9,7 +9,13 @@ using BenchmarkDotNet.Running;
 using Perfolizer.Horology;
 using System.Runtime.Intrinsics;
 
-var enough = Job.Default
+var net70 = Job.Default
+    .WithRuntime(CoreRuntime.Core70)
+    .WithWarmupCount(1)
+    .WithIterationTime(TimeInterval.FromSeconds(0.25))
+    .WithMaxIterationCount(20);
+
+var net80 = Job.Default
     .WithRuntime(CoreRuntime.Core80)
     .WithWarmupCount(1)
     .WithIterationTime(TimeInterval.FromSeconds(0.25))
@@ -22,18 +28,23 @@ var config = DefaultConfig.Instance
     .AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig
         (exportGithubMarkdown: true, printInstructionAddresses: false)))
     .AddExporter(MarkdownExporter.GitHub)
-    .AddJob(enough.WithEnvironmentVariable("DOTNET_EnableHWIntrinsic", "0").WithId("Scalar").AsBaseline());
+    .AddJob(net70.WithEnvironmentVariable("DOTNET_EnableHWIntrinsic", "0").WithId(".NET 7 Scalar").AsBaseline())
+    .AddJob(net80.WithEnvironmentVariable("DOTNET_EnableHWIntrinsic", "0").WithId(".NET 8 Scalar"));
 
 if (Vector256.IsHardwareAccelerated)
 {
     config = config
-        .AddJob(enough.WithId("Vector256"))
-        .AddJob(enough.WithEnvironmentVariable("DOTNET_EnableAVX2", "0").WithId("Vector128"));
+        .AddJob(net70.WithId(".NET 7 Vector256"))
+        .AddJob(net80.WithId(".NET 8 Vector256"))
+        .AddJob(net70.WithEnvironmentVariable("DOTNET_EnableAVX2", "0").WithId(".NET 7 Vector128"))
+        .AddJob(net80.WithEnvironmentVariable("DOTNET_EnableAVX2", "0").WithId(".NET 8 Vector128"));
 
 }
 else if (Vector128.IsHardwareAccelerated)
 {
-    config = config.AddJob(enough.WithId("Vector128"));
+    config = config
+        .AddJob(net70.WithId(".NET 7 Vector128"))
+        .AddJob(net80.WithId(".NET 8 Vector128"));
 }
 
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
